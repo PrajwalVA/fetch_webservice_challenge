6604a2fcd9926faa20317a44fb21fe2267a7401d
Prajwal <prajwal.va2008@gmail.com>
2025-02-04 16:58:57 -0700

    Added points logic

--- main.py
+++ main.py
@@ -20,11 +20,42 @@ class Receipt(BaseModel):
     items: List[Item]
     total: str = Field(..., pattern=r"^\d+\.\d{2}$")
 
+def calculate_points(receipt: Receipt) -> int:
+    points = 0
+
+    #distinct characters    
+    points += sum(c.isalnum() for c in receipt.retailer)
+    
+    #total based logic
+    total_float = float(receipt.total)
+    if total_float.is_integer():
+        points += 50
+    if total_float % 0.25 == 0:
+        points += 25
+    
+    #items in receipt
+    points += (len(receipt.items) // 2) * 5
+
+    #shortDescription length /3 -> points += price * 0.2
+    for item in receipt.items:
+        if len(item.shortDescription.strip()) % 3 == 0:
+            points += ceil(float(item.price) * 0.2)
+
+    #Day is Odd number 
+    if receipt.purchaseDate.day % 2 != 0:
+        points += 6
+
+    #Time is between 2 pm and 4 pm
+    if 14 < receipt.purchaseTime.hour < 16:
+        points += 10
+    
+    return points
+
 #POST
 @app.post("/receipts/process")
 async def process_receipt(receipt: Receipt):
     receipt_id = str(uuid.uuid4())
-    points = 0
+    points = calculate_points(receipt)
     receipts_db[receipt_id] = points
     return {"id": receipt_id}
 
